b1b24d6da9e536a569d1c83fc634485148de7ccf
dragusheen <nathan.tirolf@epitech.eu>
2025-02-22 17:11:46 +0100

    [~] move from js to ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "WebTools",
+  base: "/WebTools/",
   server: {
     open: true,
-    port: 3001,
-  }
+    port: 4666,
+  },
 })
